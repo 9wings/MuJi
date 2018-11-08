001ea052e53e93a1b2121175b714a3ed48e728d1
Manish Aryal <manish.aryal@tufts.edu>
2018-11-07 23:22:41 -0500

    Deploying app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require("path")
+const PORT = process.env.PORT || 5000
 
 app.get('/',
 (req, res) => {
@@ -18,4 +19,4 @@ app.get("/feeling",
   res.sendFile(path.join(__dirname+'/feeling.html'));
 });
 
-app.listen(8080, '127.0.0.1')
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
