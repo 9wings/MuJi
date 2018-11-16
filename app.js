f4d9871fe0ba440c5afd7471a5bf17ebc3514b2e
Naoki Okada <naokiokada11@gmail.com>
2018-11-16 14:32:14 -0500

    Added Node Spotify API package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require("path")
+var SpotifyWebApi = require('spotify-web-api-node');
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/Script'));
@@ -10,15 +11,14 @@ app.get('/',
   res.sendFile('/View/homepage.html', { root: __dirname });
 });
 
+app.get("/about",
+(req,res) => {
+  res.sendFile('/View/about.html', { root: __dirname });
+});
+
 app.get("/detection",
 (req,res) => {
   res.sendFile('/View/detection.html', { root: __dirname });
-});
-
-
-app.get("/feeling",
-(req,res) => {
-  res.sendFile('/feeling.html', { root: __dirname });
 });
 
 app.post("/audiofile",
@@ -32,11 +32,29 @@ app.post("/audiofile",
     b. or, sennd the emotion signal and create the playlist in the frontend
   */
   var audiofile = req.body;
-  
-
-
 });
 
+// create a playlist randomly and send it to frontend based on get request. 
+// create a get request that sends the response based on request.body. 
+// request.body will contain emotion. for now send a random playlist url
+
+app.get("/spot",
+(req,res) => {
+  var spotifyApi = new SpotifyWebApi({
+    clientId: 'dbf8b5e4053b4e6dac2ebbead378ee3d',
+    clientSecret: '97ee8cd484c74d9aa7ecf520eaca71cf'
+    redirectUri: ''
+  });
+
+  spotifyApi.createPlaylist('My Cool Playlist', { 'public' : false })
+  .then(function(data) {
+    console.log('Created playlist!');
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+  res.send([]);
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
